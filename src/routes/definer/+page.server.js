a41b912b108dff04e2b7590175144d9c38cfc8fc
Jhon Briones <joanbric@gmail.com>
2024-03-25 20:39:31 +0000

    Refactoring code

--- src/routes/definer/+page.server.js
+++ src/routes/definer/+page.server.js
@@ -1,55 +1,47 @@
-
 // import imgToTxt from '$lib/utils/imgToTxt';
 
-import { createWorker} from "tesseract.js";
-
-async function imgToTxt(img) {
-    
-    const worker = await createWorker("eng");
-    const ret = await worker.recognize(img);
-    worker.terminate();
-    return ret;
-
-}
+import { createWorker } from 'tesseract.js';
 
 /** @type {import('./$types').Actions} */
 export const actions = {
-	default: async ({request}) => {
-        try{
-            const data = await request.formData();
-            
-        if(!data.has('imgBase64')) return
-        const img = data.get('imgBase64');
-        const originalImage = data.get('imgFile');
-        const ret = await imgToTxt(img);
-        const {
-            data: { words }
-        } = ret;
-    
-        const text = words
-            .filter((word) => word.confidence > 45)
-            .map((word) => {
-                return {
-                    text: word.text,
-                    baseline: word.baseline,
-                    font_size: word.font_size
-                };
-            });
-        
-        return {
-            status:{
-                code: 200
-            },
-            img: originalImage,
-            text,
-        }
-    }catch(err){
-        return {
-            status:{
-                code: 500,
-                msg: err.message
-            },
-        }
-    }
+	default: async ({ request }) => {
+		try {
+			const data = await request.formData();
+
+			if (!data.has('imgBase64')) return;
+			const img = data.get('imgBase64');
+			const originalImage = data.get('imgFile');
+			const worker = await createWorker('eng');
+			const ret = await worker.recognize(img);
+			worker.terminate();
+			const {
+				data: { words }
+			} = ret;
+
+			const text = words
+				.filter((word) => word.confidence > 45)
+				.map((word) => {
+					return {
+						text: word.text,
+						baseline: word.baseline,
+						font_size: word.font_size
+					};
+				});
+
+			return {
+				status: {
+					code: 200
+				},
+				img: originalImage,
+				text
+			};
+		} catch (err) {
+			return {
+				status: {
+					code: 500,
+					msg: err.message
+				}
+			};
+		}
 	}
 };
